83c030eacbe566759706d67f8eb06037cbda8a56
Igor Cassolli <37945750+IgorCassolli@users.noreply.github.com>
2022-11-16 00:16:47 -0300

    commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -29,7 +29,7 @@ export function AuthContextProvider({ children } : AuthProviderProps){
     const [isUserLoading, setIsUserLoading] = useState(false);
 
     const [request, response, promptAsync] =  Google.useAuthRequest({
-        clientId: '972102466740-u9q5f170dlulbmr77endn90hsp95ccad.apps.googleusercontent.com',
+        clientId: process.env.CLIENT_ID,
         redirectUri: AuthSession.makeRedirectUri({useProxy: true}),
         scopes: ['profile', 'email']
     })
